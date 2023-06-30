db0da4f9586443e8c14fb1f90691af5d4bc3445c
JosieR412 <81095714+JosieR412@users.noreply.github.com>
2023-06-30 16:55:52 +0800

    Add files via upload

---
+++ src/main/java/com/voidblog/domain/Admin.java
@@ -0,0 +1,54 @@
+package com.blog.domain;
+
+public class Admin {
+    private Integer id;
+
+	private String username;
+
+    private String password;
+    
+    private String link;
+    
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+    
+    public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
+}
